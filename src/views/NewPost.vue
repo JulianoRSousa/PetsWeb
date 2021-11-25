43929372220c868bbeca9281756f38e2f1755562
julianorodsousa <julianorodsousa@gmail.com>
2021-11-25 10:16:11 -0400

    NewPost style step three

--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -6,41 +6,50 @@
 
     <main class="pageNewPost">
       <div class="formTitle">Criar publicação</div>
-      <div class="container">
+      <form class="container">
         <div>
           <div class="headerInfo">
-            <div class="topHeaderInfo">
-              <img class="userImage" :src="loginInfo.user.picture_url" />
-              <div class="internHeaderContainer">
-                {{
-                  this.loginInfo.user.firstName +
-                    " " +
-                    this.loginInfo.user.lastName
-                }}
-                <select class="selectorState">
-                  <option class="firstOption" value="lose"
-                    >eu perdi meu pet</option
-                  >
-                  <option class="secondOption" value="found"
-                    >eu encontrei um pet</option
-                  >
-                  <option class="thirdOption" value="adoption"
-                    >tenho um pet para adoção</option
-                  >
-                </select>
-              </div>
-            </div>
-            <select class="petSelector" id="petList">
-              <option value="nu1">escolha um de seus pets:</option>
-              <option value="anotherPet">Adicionar um novo pet</option>
+            <label for="selectorState">Qual a situação?</label>
+
+            <select name="selectorState" class="selectorState">
+              <option class="firstOption" value="1">eu perdi meu pet</option>
+              <option class="secondOption" value="2"
+                >eu encontrei um pet</option
+              >
+              <option class="thirdOption" value="3"
+                >tenho um pet para adoção</option
+              >
             </select>
-            <span>Ou adicione um abaixo</span>
+            <label v-if="petList.length > 0" for="petSelector">
+              Escolha um de seus pets:
+              <select
+                class="petSelector"
+                id="petListSelector"
+                v-model="petSelector"
+              >
+              </select>
+            </label>
+            <span>Adicione um novo pet abaixo:</span>
           </div>
         </div>
         <div>
           <label class="lblPetName" for="petName">
             Nome do pet:
-            <input id="petName" placeholder="ex: Brutus" type="text" />
+            <input
+              class="petNameInput"
+              v-if="petSelector == 1"
+              placeholder="ex: Brutus"
+              type="text"
+              v-model="petName"
+              disabled
+            />
+            <input
+              v-else
+              class="petNameInput"
+              placeholder="ex: Brutus"
+              type="text"
+              v-model="petName"
+            />
           </label>
           <label class="lblPostDescription" for="postDescription">
             Descrição:
@@ -48,18 +57,72 @@
               id="postDescription"
               placeholder="ex: encontrado proximo a avenida ..."
               type="text"
+              maxlength="160"
+              v-model="postDescription"
             />
           </label>
         </div>
         <footer>
           <label class="lblImagePicker" for="inputImage">
             Adicione uma foto:
-            <input id="imagePicker" type="image" />
+            <input
+              id="imagePicker"
+              type="file"
+              accept="image/png, image/jpeg"
+              placeholder="teste"
+              required
+            />
           </label>
         </footer>
-      </div>
+      </form>
     </main>
-    <footer></footer>
+    <footer>
+      <div class="postContainer">
+        <header id="headerUserInfo">
+          <img :src="userInfo.picture_url" alt="User image" class="userImage" />
+          <div class="userInfo">
+            <span>{{ userInfo.firstName }} {{ userInfo.lastName }}</span>
+            <span class="username">@{{ userInfo.username }}</span>
+          </div>
+          <div class="optionIconExample">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              aria-hidden="true"
+              role="img"
+              width="1em"
+              height="1em"
+              preserveAspectRatio="xMidYMid meet"
+              viewBox="0 0 24 24"
+            >
+              <path d="M10 10h4v4h-4zm6 0h4v4h-4zM4 10h4v4H4z" fill="#292929" />
+            </svg>
+          </div>
+        </header>
+        <main class="mainContent">
+          <div class="mainInternalDiv">
+            <img class="mainInternalImage" alt="Pet image" src="" />
+            <span class="mainInternalDate">12 de Julho de 2021</span>
+          </div>
+          <div class="mainInternalDiv">
+            <span class="petName">{{ petName }}</span>
+            <span class="description">{{ postDescription }}</span>
+            <div v-if="petState == 1" class="infoStateContainer colorStateBlue">
+              <span class="infoStateText">encontrado</span>
+            </div>
+            <div
+              v-else-if="petState == 2"
+              class="infoStateContainer colorStateRed"
+            >
+              <span class="infoStateText">perdido</span>
+            </div>
+            <div v-else class="infoStateContainer">
+              <span class="infoStateText">adoção</span>
+            </div>
+          </div>
+        </main>
+      </div>
+    </footer>
   </body>
 </template>
 
@@ -73,13 +136,19 @@ export default {
     HeaderPets,
   },
   name: "NewPost",
-  mounted() {
+  created: function() {
+    this.userInfo = PetsLocalStorage.getItem("loginInfo").user;
     this.loginInfo = PetsLocalStorage.getItem("loginInfo");
+    this.petList = PetsLocalStorage.getItem("loginInfo").user.petList;
   },
-  watch: {},
   data() {
     return {
-      loginInfo: { user: {} },
+      petSelector: 0,
+      petName: "",
+      postDescription: "",
+      userInfo: {},
+      loginInfo: {},
+      petList: [],
       petState: 0,
     };
   },
@@ -102,7 +171,7 @@ export default {
 }
 .container {
   background-color: white;
-  width: 50%;
+  width: max-content;
   border-radius: 2rem;
   place-items: center;
   padding: 2rem;
@@ -133,6 +202,7 @@ export default {
   border-radius: 1rem;
   border: 1px solid #292929;
   padding: 3px;
+  margin-bottom: 1rem;
   font-family: "Delius";
 }
 .firstOption {
@@ -157,25 +227,27 @@ export default {
   padding-bottom: 1rem;
   color: #292929;
 }
-#petName {
+.petNameInput {
   display: flex;
   flex: 1;
   border-radius: 1rem;
   font-family: "Delius";
+  font-size: 1rem;
   border: 1px solid #292929;
   padding: 3px;
   padding-left: 0.8rem;
   margin-left: 1rem;
 }
-.lblPostDescription{
+.lblPostDescription {
   display: flex;
   padding-top: 1rem;
   padding-bottom: 1rem;
   color: #292929;
 }
-#postDescription{
+#postDescription {
   display: flex;
   flex: 1;
+  height: 4rem;
   border-radius: 1rem;
   font-family: "Delius";
   border: 1px solid #292929;
@@ -183,13 +255,13 @@ export default {
   padding-left: 0.8rem;
   margin-left: 1rem;
 }
-.lblImagePicker{
-   display: flex;
+.lblImagePicker {
+  display: flex;
   padding-top: 1rem;
   padding-bottom: 1rem;
   color: #292929;
 }
-#imagePicker{
+#imagePicker {
   display: flex;
   flex: 1;
   border-radius: 1rem;
@@ -198,5 +270,11 @@ export default {
   padding: 3px;
   padding-left: 0.8rem;
   margin-left: 1rem;
+}
+
+.optionIconExample {
+  display: grid;
+  padding: 0.5rem;
+  place-items: flex-start;
 }
 </style>
